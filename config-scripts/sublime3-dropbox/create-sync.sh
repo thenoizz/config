abb8ac4ac8335510a015b382cb994dafc46a4028
Dorin Paslaru <dpaslaru@cloudbasesolutions.com>
2014-09-10 20:28:04 +0300

    Fixes

--- config-scripts/sublime3-dropbox/create-sync.sh
+++ config-scripts/sublime3-dropbox/create-sync.sh
@@ -2,13 +2,13 @@
 set -x
 
 echo "Create the sync directory in Dropbox"
-$ mkdir ~/Dropbox/Apps/sublime-text-3/
+mkdir ~/Dropbox/Apps/sublime-text-3/
 
 echo "Move your ST3 Packages and Installed Packages to Dropbox"
-$ cd ~/Library/Application\ Support/Sublime\ Text\ 3
-$ mv Packages/ ~/Dropbox/Apps/sublime-text-3/
-$ mv Installed\ Packages/ ~/Dropbox/Apps/sublime-text-3/
+cd ~/Library/Application\ Support/Sublime\ Text\ 3
+mv Packages/ ~/Dropbox/Apps/sublime-text-3/
+mv Installed\ Packages/ ~/Dropbox/Apps/sublime-text-3/
 
 echo "Then symlink your Dropbox directories back locally"
-$ ln -s ~/Dropbox/Apps/sublime-text-3/Packages
-$ ln -s ~/Dropbox/Apps/sublime-text-3/Installed\ Packages
+ln -s ~/Dropbox/Apps/sublime-text-3/Packages
+ln -s ~/Dropbox/Apps/sublime-text-3/Installed\ Packages
--- config-scripts/sublime3-dropbox/get-sync.sh
+++ config-scripts/sublime3-dropbox/get-sync.sh
@@ -2,10 +2,10 @@
 set -x
 
 echo "Remove the 'outdated' directories"
-$ cd ~/Library/Application\ Support/Sublime\ Text\ 3
-$ rm -rf Packages/
-$ rm -rf Installed\ Packages/
+cd ~/Library/Application\ Support/Sublime\ Text\ 3
+rm -rf Packages/
+rm -rf Installed\ Packages/
 
 echo "Then symlink your Dropbox directories back locally"
-$ ln -s ~/Dropbox/Apps/sublime-text-3/Packages
-$ ln -s ~/Dropbox/Apps/sublime-text-3/Installed\ Packages
+ln -s ~/Dropbox/Apps/sublime-text-3/Packages
+ln -s ~/Dropbox/Apps/sublime-text-3/Installed\ Packages
